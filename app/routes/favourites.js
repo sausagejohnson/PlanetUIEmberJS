3b2717faa49771ecfeefa8bb5b9190b6ef762823
sausagejohnson <sausage@waynejohnson.net>
2022-03-08 22:08:26 +1100

    Data for favourites view.

---
+++ app/routes/favourites.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+
+export default class FavouritesRoute extends Route {
+    
+    async model(){
+        return {
+            favouritePlanets: [
+                {
+                    id:	0,
+                    name: "TOI 700 d",
+                    environment: "Harsh",
+                    resources: [
+                        "Gold",
+                        "Iron"
+                    ]
+                }
+            ]
+        };
+    }
+}
